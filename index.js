989971d90d94024c7aaf8537bd0d21976ea997ef
Van-Nice <wilsonwalter808@gmail.com>
2024-06-14 16:14:55 -0600

    Create url name query parameter for GET request endpoint that returns information for a pokemon from sqlite database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const url = require('url');
 const db = require('better-sqlite3')('data/pokemon.db');
 db.pragma('journal_mode = WAL');
 
@@ -7,8 +7,21 @@ const host = 'localhost';
 const port = 8000;
 
 const requestListener = function(req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    // TODO: log just the name
+    const name = queryObject.name;
+    const nameQuery = name.charAt(0).toUpperCase() + name.slice(1).toLocaleLowerCase();
+    console.log(nameQuery);
+    // Possible endpoint parameters: name, type1, type2, total, hp, attack, defense, special-atk, special-def, speed, generation, legendary
+    // Just start with name
+    const dbObject = db.prepare("SELECT * FROM pokemon WHERE Name = ?").get(nameQuery);
+    console.log(dbObject);
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
+    // TODO: figure out how to send a GET request to this end point ->
+    // TODO: Then figure out how to take query parameters and turn them to variables ->
+    // TODO: Use those parameters to format your database query
+
     res.end(`{"message": "This is a JSON response"}`);
 };
 
